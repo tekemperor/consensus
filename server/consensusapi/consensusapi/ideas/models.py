80ec7463114669682a3644ba0cd3675d05beab53
Brian Lindsay <tekemperor@gmail.com>
2016-11-25 19:50:07 -0700

    Minimal idea object working in API.

--- server/consensusapi/consensusapi/ideas/models.py
+++ server/consensusapi/consensusapi/ideas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Idea(models.Model):
+	author = models.ForeignKey('auth.User', related_name='ideas')
+	created = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=128, blank=True, default='')
+	content = models.TextField()
+
+	class Meta:
+		ordering = ('created',)
--- server/consensusapi/consensusapi/ideas/views.py
+++ server/consensusapi/consensusapi/ideas/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from consensusapi.ideas.serializers import IdeaSerializer
+from consensusapi.ideas.models import Idea
+
+
+
+class IdeaViewSet(viewsets.ModelViewSet):
+    """
+    This endpoint presents ideas.
+
+    """
+    serializer_class = IdeaSerializer
+    queryset = Idea.objects.all()
+    
+    
